e27ce9ef351421c48174abfd1150d4b78293e9f5
Jonathan Goldman <jonathan@unicyclelabs.com>
2016-04-28 20:55:17 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apilib',
+    description='A library for defining APIs that will serialize into JSON.',
+    author='Jonathan Goldman',
+    author_email='jonathan@unicyclelabs.com',
+    url='https://github.com/UnicycleLabs/apilib',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['python-dateutil', 'requests'],
+    extras_require={'encrypted-ids': ['hashids']},
+    tests_require=['mock'],
+    test_suite='tests.all_tests')
